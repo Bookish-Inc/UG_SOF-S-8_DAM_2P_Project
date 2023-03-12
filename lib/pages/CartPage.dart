21fbd85581df89dbaecc9e755fa9d74476af2b37
NexBorn <nefi.reyest@ug.edu.ec>
2023-03-11 21:14:11 -0500

    Fix bugs

--- lib/pages/CartPage.dart
+++ lib/pages/CartPage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:xml/xml.dart' as xml;
+
+import 'Cart/Libro.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({Key? key}) : super(key: key);
@@ -8,10 +11,62 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+  List<Libro> libros = [];
+  @override
+  void initState() {
+    super.initState();
+    _leerLibros();
+  }
+
+  Future<void> _leerLibros() async {
+    final data =
+    await DefaultAssetBundle.of(context).loadString('assets/libros.xml');
+
+    final document = xml.parse(data);
+    final elementos = document.findAllElements('libro');
+
+    setState(() {
+      libros = elementos.map((elemento) {
+        final titulo = elemento.findElements('titulo').single.text;
+        final autor = elemento.findElements('autor').single.text;
+        final precio =
+        double.parse(elemento.findElements('precio').single.text);
+        return Libro(titulo: titulo, autor: autor, precio: precio);
+      }).toList();
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Cart", style: TextStyle(fontSize: 30),),
+    return MaterialApp(
+      title: "Bookis - Cart",
+      home: Scaffold(
+        body: ListView.builder(
+          itemCount: libros.length,
+          itemBuilder: (context, index) {
+            return Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Card(
+                child: ListTile(
+                  title: Text(
+                    libros[index].titulo,
+                    style:  const TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Autor: ${libros[index].autor}'),
+                      Text(
+                        'Precio: \$${libros[index].precio.toStringAsFixed(2)}',
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
     );
   }
 }
